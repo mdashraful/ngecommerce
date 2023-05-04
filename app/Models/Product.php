946381967ea8f875641a1c4ba4222a595f34fd8d
unknown <mdashrafulislam369@gmail.com>
2023-05-04 12:08:39 +0600

    MediaLibrary installed and use in Product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,11 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-
-class Product extends Model
+class Product extends Model implements HasMedia
 {
-
+    use InteractsWithMedia;
     use HasFactory;
 
     protected $guarded = [];
